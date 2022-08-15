b783a33930e809df46b8baf76ffd0c1b826137b5
normal-account <74117688+normal-account@users.noreply.github.com>
2022-08-15 15:20:58 -0400

    Remove methods used during development.

--- main.cpp
+++ main.cpp
@@ -28,76 +28,8 @@ void signalHandler( int signum ) {
 }
 
 
-
-int test(bool weighted=false) {
-    Chrono chrono("c Total time");
-
-    std::cout << "Time (sec) \t File" << std::endl;
-    for(unsigned int id = 0 ; id < (weighted?data_weighted.size():data_unweighted.size()); id++) {
-        srand(0);
-        std::cout << (weighted?data_weighted[id]:data_unweighted[id]) << "\t" << std::flush;
-
-        monMaxSat = new EvalMaxSAT();
-
-        std::string filePath = (weighted?BENCHMARK_FILES_FOLDER_WEIGHTED:BENCHMARK_FILES_FOLDER_UNWEIGHTED) + (weighted?data_weighted[id]:data_unweighted[id]); // For a custom path
-
-        MaLib::Chrono C;
-
-
-        if(!monMaxSat->parse(filePath)) { // TODO : rendre robuste au header mismatch
-            std::cerr << "Impossible de lire le fichier : " << filePath << std::endl;
-            assert(false);
-            return -1;
-        }
-
-        if(!monMaxSat->solve()) {
-            std::cerr << "Pas de solution ?!?" << std::endl;
-            assert(false);
-            return -1;
-        }
-
-        if( monMaxSat->getCost() != (weighted?data_weighted_cost[id]:data_unweighted_cost[id])) {
-            std::cerr << "id = " << id << std::endl;
-            std::cerr << "file = " << filePath << std::endl;
-            std::cerr << "Résultat éroné : \n   Trouvé : " << monMaxSat->getCost() << "\n  Attendu : " << (weighted?data_weighted_cost[id]:data_unweighted_cost[id]) << std::endl;
-            std::vector<bool> assign;
-            assign.push_back(0); // fake var_0
-
-            for(unsigned int i=1; i<=monMaxSat->nInputVars; i++) {
-                assign.push_back(monMaxSat->getValue(i));
-            }
-
-            std::cerr << " RealCost = " << calculateCost( filePath, assign) << std::endl;
-
-            assert(false);
-            return -1;
-        } else {
-
-            std::vector<bool> assign;
-            assign.push_back(0); // fake var_0
-            for(unsigned int i=1; i<=monMaxSat->nInputVars; i++) {
-                assign.push_back(monMaxSat->getValue(i));
-            }
-
-            if( calculateCost( filePath, assign) != monMaxSat->getCost() ) {
-                std::cerr << "o Error: " << calculateCost( filePath, assign) << " != " << monMaxSat->getCost() << std::endl;
-            }
-//            assert( calculateCost( filePath, assign) == monMaxSat->getCost() );
-        }
-        std::cout << C.tacSec() << std::endl;
-        delete monMaxSat;
-    }
-    return 0;
-}
-
 int main(int argc, char *argv[])
 {
-    if(argc==1) {
-        // TODO : cette section est juste pour le dévelopement
-        bool TESTER_LE_CAS_WEIGHTED = true;
-        return test( TESTER_LE_CAS_WEIGHTED );
-    }
-
     Chrono chrono("c Total time");
     signal(SIGINT, signalHandler);
     signal(SIGTERM, signalHandler);
